9351df303c7195cd7ae0c2a4e2de75505329f623
Kaiselion <perez.jhonattan@gmail.com>
2025-04-18 00:43:31 -0400

    feat: add cart functionality with nanostores and cookie management

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,4 +1,5 @@
 import { loginUser, logout, registerUser } from './auth'
+import { loadProductsFromCart } from './cart'
 import { getProductBySlug } from './products/get-product-by-slug.action'
 import { getProductsByPage } from './products/get-products-by-page.action'
 
@@ -10,5 +11,6 @@ export const server = {
 	logout,
 	registerUser,
 	getProductsByPage,
-	getProductBySlug
+	getProductBySlug,
+	loadProductsFromCart
 }
