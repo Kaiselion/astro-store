343533d4fa2f3daa16adc5d4cb23af4d73417f0e
Kaiselion <perez.jhonattan@gmail.com>
2025-04-23 12:54:01 -0400

    Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,18 +6,18 @@ import tailwindcss from '@tailwindcss/vite'
 import netlify from '@astrojs/netlify'
 // import auth from 'auth-astro'
 
-import db from '@astrojs/db'
-
 import react from '@astrojs/react'
 
 import auth from 'auth-astro'
 
+import db from '@astrojs/db';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [db(), react(), auth()],
-	vite: {
-		plugins: [tailwindcss()]
-	},
-	output: 'server',
-	adapter: netlify()
+    integrations: [react(), auth(), db()],
+    vite: {
+        plugins: [tailwindcss()]
+    },
+    output: 'server',
+    adapter: netlify()
 })
